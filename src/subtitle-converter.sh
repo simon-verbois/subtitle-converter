e7124f56c6029e7420c75869ea2c0281f1aa3ee0
Simon Verbois <simon.verbois@gmail.com>
2023-04-24 12:33:27 +0200

    fix

--- src/subtitle-converter.sh
+++ src/subtitle-converter.sh
@@ -1,52 +1,18 @@
 #!/bin/bash
 
 
-ConvertSubtitles() {
-    if [[ "$track" =~ "S_HDMV/PGS" ]]; then
-        echo > /dev/null
-    elif [[ "$track" =~ "S_VOBSUB" ]]; then
-        echo > /dev/null
-    else
-        ffmpeg -loglevel 0 -i $tmp_file -y -f srt $tmp_file.srt
-    fi
-    
-    rm -f $tmp_file
-}
-
-
-CleanFile() {
-    mkvmerge -q -o $mkv_file-new.mkv --no-subtitles $mkv_file
-}
-
-
-AddSubtitles() {
-    srt_files=$(find $SC_TMP -name "$filename*.srt" -type f)
+ScanFolders() {
+    mkv_files=$(find "./data" -name "*.mkv" -type f)
     IFS=$'\n'
-    for srt_file in $srt_files; do
-        track_id=$(echo $srt_file | rev | cut -d'-' -f 1 | rev | cut -d'.' -f 1)
-        language=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 11 | grep "Language:" | awk '{print $4}')
-        name=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 11 | sed -n 's/.*Name: \(.*\)/\1/p')
-
-        if [[ -z "$language" ]]; then
-            language="$SC_PREF_LANG"
-        fi
-        if [[ -z "$name" ]]; then
-            name="$SC_PREF_LANG_FULL"
-        fi
-        
-        content=$(cat "$srt_file" | awk '{gsub(/<[^>]*>/,"")};1' | awk '!/^[0-9]+$/ && !/^[0-9]+:[0-9]+:[0-9]+,[0-9]+ --> [0-9]+:[0-9]+:[0-9]+,[0-9]+$/' | awk '{gsub(/{.*}/,"")};1' | sed '/^$/d')
-        detected_language=$(echo "$content" | python3 -c "from langdetect import detect; import sys; print(detect(sys.stdin.read()))")
-
-        if [[ "$detected_language" != "$SC_PREF_LANG" ]]; then
-            Logger "(DEBUG) Remove $srt_file ($detected_language) from $filename" "debug"
-        else
-            Logger "(DEBUG) Add $srt_file ($detected_language) to $filename" "debug"
-            mkvmerge -q -o "$mkv_file-sub.mkv" "$mkv_file-new.mkv" --language 0:$language --sub-charset 0:UTF-8 --track-name 0:"$name" "$srt_file"
-            mv -f "$mkv_file-sub.mkv" "$mkv_file-new.mkv"
-        fi
-        
-        rm -f $srt_file
+    for mkv_file in $mkv_files; do
+        GetTracksInfo
+        Logger "(INFO) Complete : $mkv_file"
     done
+
+    Logger "(INFO) Everythings is done"
+    printf '%s\n' '------------------------------------'
+    
+    Main
 }
 
 
@@ -61,31 +27,41 @@ GetTracksInfo() {
         to_clean="false"
         reason="Already cleaned"
         track_id=$(echo "$track" | awk '{print $3}' | tr -d ":")
-        language=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 10 | grep "Language:" | awk '{print $4}')
-        #language_forced=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 10 | grep -i "forced" | grep -v -i "name")
+        track_info=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 10)
+        language=$(echo "$track_info" | grep "Language:" | awk '{print $4}')
+        #language_forced=$(echo "$track_info" | grep -i "forced" | grep -v -i "name")
         
-        if [[ -z "$track" ]]; then
+        if [[ "$track" == "" ]]; then
             reason="No subtitles detected"
             break
         fi
         
-        if [[ "$language" != *"$SC_PREF_LANG"* || -z "$language" ]]; then
-            to_clean="true"
-            break
+        if [[ "$language" != *"$SC_PREF_LANG"* || "$language" == "" ]]; then
+            Logger "$track_info" "debug"
+            if [[ "$track_info" =~ "S_VOBSUB" ]]; then
+                reason="S_VOBSUB detected"
+                break
+            elif [[ "$track_info" =~ "S_HDMV/PGS" ]]; then
+                reason="S_HDMV/PGS detected"
+                break
+            else
+                to_clean="true"
+                break
+            fi
         fi
         
-        if [[ "$track" =~ "S_TEXT/UTF8" ]]; then
+        if [[ "$track_info" =~ "S_TEXT/UTF8" ]]; then
             continue
-        elif [[ "$track" =~ "S_TEXT/ASS" ]]; then
+        elif [[ "$track_info" =~ "S_TEXT/ASS" ]]; then
             to_clean="true"
             break
-        elif [[ "$track" =~ "S_TEXT/USF" ]]; then
+        elif [[ "$track_info" =~ "S_TEXT/USF" ]]; then
             to_clean="true"
             break
-        elif [[ "$track" =~ "S_VOBSUB" ]]; then
+        elif [[ "$track_info" =~ "S_VOBSUB" ]]; then
             reason="S_VOBSUB detected"
             break
-        elif [[ "$track" =~ "S_HDMV/PGS" ]]; then
+        elif [[ "$track_info" =~ "S_HDMV/PGS" ]]; then
             reason="S_HDMV/PGS detected"
             break
         fi
@@ -135,18 +111,52 @@ ExtractSubtitles() {
 }
 
 
-ScanFolders() {
-    mkv_files=$(find "./data" -name "*.mkv" -type f)
-    IFS=$'\n'
-    for mkv_file in $mkv_files; do
-        GetTracksInfo
-        Logger "(INFO) Complete : $mkv_file"
-    done
-
-    Logger "(INFO) Everythings is done"
-    printf '%s\n' '------------------------------------'
+ConvertSubtitles() {
+    if [[ "$track" =~ "S_HDMV/PGS" ]]; then
+        echo > /dev/null
+    elif [[ "$track" =~ "S_VOBSUB" ]]; then
+        echo > /dev/null
+    else
+        ffmpeg -loglevel 0 -i $tmp_file -y -f srt $tmp_file.srt
+    fi
     
-    Main
+    rm -f $tmp_file
+}
+
+
+CleanFile() {
+    mkvmerge -q -o $mkv_file-new.mkv --no-subtitles $mkv_file
+}
+
+
+AddSubtitles() {
+    srt_files=$(find $SC_TMP -name "$filename*.srt" -type f)
+    IFS=$'\n'
+    for srt_file in $srt_files; do
+        track_id=$(echo $srt_file | rev | cut -d'-' -f 1 | rev | cut -d'.' -f 1)
+        language=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 11 | grep "Language:" | awk '{print $4}')
+        name=$(mkvinfo $mkv_file | grep "(track ID for mkvmerge & mkvextract: $track_id)" -A 11 | sed -n 's/.*Name: \(.*\)/\1/p')
+
+        if [[ "$language" == "" ]]; then
+            language="$SC_PREF_LANG"
+        fi
+        if [[ "$name" == "" ]]; then
+            name="$SC_PREF_LANG_FULL"
+        fi
+        
+        content=$(cat "$srt_file" | awk '{gsub(/<[^>]*>/,"")};1' | awk '!/^[0-9]+$/ && !/^[0-9]+:[0-9]+:[0-9]+,[0-9]+ --> [0-9]+:[0-9]+:[0-9]+,[0-9]+$/' | awk '{gsub(/{.*}/,"")};1' | sed '/^$/d')
+        detected_language=$(echo "$content" | python3 -c "from langdetect import detect; import sys; print(detect(sys.stdin.read()))")
+
+        if [[ "$detected_language" != "$SC_PREF_LANG" ]]; then
+            Logger "(DEBUG) Delete $srt_file ($detected_language)" "debug"
+        else
+            Logger "(DEBUG) Add $srt_file ($detected_language)" "debug"
+            mkvmerge -q -o "$mkv_file-sub.mkv" "$mkv_file-new.mkv" --language 0:$language --sub-charset 0:UTF-8 --track-name 0:"$name" "$srt_file"
+            mv -f "$mkv_file-sub.mkv" "$mkv_file-new.mkv"
+        fi
+        
+        rm -f $srt_file
+    done
 }
 
 
